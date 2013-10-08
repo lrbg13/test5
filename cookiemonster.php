02645d72a44464f84b3a1f7581403a67c8e4066f
aluadm <aluadm@ORD-COI-SIM-19>
2013-10-08 19:31:34 +0200

    commit inicio2

---
+++ cookiemonster.php
@@ -0,0 +1,14 @@
+<?php
+echo'<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cookie Monster</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <h1>Cookie Monster</h1>
+        <img src="images/CookieMonster.jpg"/>
+        <p><a href="index.php">Back to main page</a></p>
+    </body>
+</html>';
+?>
