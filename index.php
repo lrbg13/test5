bb80ea52b5639725dd9b01f534ae860e478b7646
aluadm <aluadm@ORD-COI-SIM-19>
2013-10-08 19:27:09 +0200

    commit inicio

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo'<!DOCTYPE html>
+<html>
+    <head>
+        <title>The Muppets</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <h1>The Muppets</h1>
+        <ul>
+            <li><a href="cookiemonster.php">Cookie Monster</a></li>
+        </ul>
+    </body>
+</html>';
+?>
